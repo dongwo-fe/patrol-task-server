61636a4ff0b88782ab490791f51bb6743c697bef
郭方超 <guofangchao@idongwo.com>
2023-09-05 12:00:46 +0800

    add test

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 前端页面巡检管理系统-服务端
 
 > 通过定时任务触发脚本，检查固定页面的各项指标。
+
+## 功能结构
+![思维导图](https://static.jrdaimao.com/cli_upload/64b4abe3-4b75-4c28-86ca-1fdc73348863.png)
---
+++ temp/test.js
@@ -0,0 +1,66 @@
+import puppeteer, { KnownDevices } from 'puppeteer-core';
+import os from 'os';
+import path from 'path';
+import minimist from 'minimist';
+
+// 当前系统所属平台，主要是兼容本地开发环境
+const platform = os.platform();
+const sleep = (time) => new Promise((ok) => setTimeout(ok, time));
+
+/**
+ * 创建一个浏览器
+ * @returns 浏览器对象
+ */
+async function createBrowser(isMobile = false) {
+    return puppeteer.launch({
+        ignoreHTTPSErrors: true,
+        headless: false,
+        devtools: true,
+        executablePath: platform === 'win32' ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' : '/usr/bin/google-chrome',
+        timeout: 60000,
+        // defaultViewport: {
+        //     // width: 375,
+        //     // height: 667,
+        //     isMobile,
+        // },
+        args: ['--no-sandbox', '--v1=1', '--disable-dev-shm-usage', '--no-first-run', '--disable-setuid-sandbox'],
+    });
+}
+
+// 请求接口并返回对应的任务信息
+async function getTaskInfo(id) {
+    return {
+        url: 'https://om.jrdaimao.com',
+    };
+}
+// 检查浏览器上的网站
+async function checkWebsite(data) {
+    const browser = await createBrowser();
+    console.log('创建浏览器');
+    const page = await browser.newPage();
+    console.log('新建页面');
+    await page.setDefaultNavigationTimeout(0);
+    // await page.emulate(KnownDevices['iPhone 7']);
+    await page.goto(data.url, {
+        waitUntil: 'load',
+        timeout: 0,
+    });
+    console.log('打开页面', data.url);
+    await sleep(1000);
+}
+/**
+ * 入口函数，接收参数并检查对应的页面
+ * 例如 node test.js --id 123
+ */
+async function Main() {
+    const params = minimist(process.argv);
+    console.log('脚本参数', params);
+    // 不存在的id
+    if (!params.id) return;
+    //id存在，获取信息
+    const data = await getTaskInfo(params.id);
+    console.log('数据', data);
+    //执行检查函数
+    await checkWebsite(data);
+}
+Main();
