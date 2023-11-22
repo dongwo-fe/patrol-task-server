86a5f53a3ad237e577161480fb5e6a58614d612b
郭方超 <guofangchao@idongwo.com>
2023-11-22 22:11:29 +0800

    add

--- src/api/notice.ts
+++ src/api/notice.ts
@@ -15,10 +15,11 @@ router.get('/api_error', async function (ctx) {
         return;
     }
     console.log(ctx.headers);
+    console.log('接受错误，来源页面', from, '来源接口', k, '错误内容', e);
     if (env === 'production' || !env) {
+        if (r.length > 10 && r.includes(`"data":null,`)) return;
         NoticeApiError(from, k, e, r, env);
     }
-    console.log('接受错误，来源页面', from, '来源接口', k, '错误内容', e);
 });
 
 router.get('/api_error_list', async function (ctx) {
