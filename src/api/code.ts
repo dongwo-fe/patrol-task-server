8d37fca87760e825bdcac8abd3041cee12bf208c
郭方超 <guofangchao@idongwo.com>
2023-11-28 10:32:50 +0800

    add

--- src/api/code.ts
+++ src/api/code.ts
@@ -1,8 +1,34 @@
 import Router from '@koa/router';
 import { BeError, BeSuccess } from '../util/response';
-import { getCodeAll } from '../service/code';
+import { getCodeAll, getCodeList, editCode } from '../service/code';
 
 const router = new Router();
+
+router.get('/', async function (ctx) {
+    const { pageindex, name, status } = ctx.query;
+    try {
+        const data = await getCodeList(pageindex, name, status);
+        ctx.body = BeSuccess(data);
+    } catch (error) {
+        ctx.body = BeError(error);
+    }
+});
+
+router.post('/edit', async function (ctx) {
+    const { id, code, name, remark, status, state } = ctx.request.body;
+    try {
+        let opts: any = {};
+        if (code) opts.code = code;
+        if (name) opts.name = name;
+        if (remark) opts.remark = remark;
+        if (status !== undefined) opts.status = status;
+        if (state !== undefined) opts.state = state;
+        const data = await editCode(opts, id);
+        ctx.body = BeSuccess(data);
+    } catch (error) {
+        ctx.body = BeError(error);
+    }
+});
 
 router.get('/all', async function (ctx) {
     try {
--- src/service/code.ts
+++ src/service/code.ts
@@ -8,3 +8,32 @@ export async function getCodeAll() {
     const data = await CodeManger.getAll();
     return data;
 }
+
+/**
+ * 获取列表，分页
+ * @param pageindex 页码
+ * @param name 名称
+ * @param status 状态
+ * @returns
+ */
+export async function getCodeList(pageindex?: string, name?: string, status?: string) {
+    let index: number | undefined = undefined;
+    if (pageindex) index = Number(pageindex);
+    let status2: number | undefined = undefined;
+    if (status) status2 = Number(status);
+    const data = await CodeManger.getList(index, name, status2);
+    return data;
+}
+/**
+ * 编辑或者新增
+ * @param data 数据
+ * @param id id
+ * @returns
+ */
+export async function editCode(data, id?: number) {
+    if (id) {
+        return CodeManger.update(data, { where: { id } });
+    } else {
+        return CodeManger.insert(data);
+    }
+}
